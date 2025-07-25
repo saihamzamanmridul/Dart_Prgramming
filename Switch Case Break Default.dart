29e83c7f4faf884814c933fa91f3383bc3204518
saihamzamanmridul <saihamzamanmridul@gmail.com>
2025-07-25 12:39:09 +0600

    Switch Case add

---
+++ Switch Case Break Default.dart
@@ -0,0 +1,17 @@
+void main() {
+  var result = 80;
+
+  switch (result) {
+    case 80:
+      print("A+");
+      break;
+
+    case 70:
+      print("A");
+      break;
+
+    default:
+      print("Fail");
+      break;
+  }
+}
